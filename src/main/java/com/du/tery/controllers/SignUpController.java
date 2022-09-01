3b353b9b85693cf4dfb402025d8922ac32cc07a7
Diana-Cristina Urcan <urcan.diana10@yahoo.com>
2022-09-01 20:40:26 +0300

    Update on appearance for Login & Signup, added controllers

---
+++ src/main/java/com/du/tery/controllers/SignUpController.java
@@ -0,0 +1,13 @@
+package com.du.tery.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignUpController {
+
+    @GetMapping("/signup")
+    public String displayLoginPage() {
+        return "sign-up-page.html";
+    }
+}

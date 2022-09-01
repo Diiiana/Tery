e962235e8ade9fb07cda564d6d37c7dc164d9203
Diana-Cristina Urcan <urcan.diana10@yahoo.com>
2022-09-01 16:03:06 +0300

    Adding initial project

---
+++ src/main/java/com/du/tery/TeryApplication.java
@@ -0,0 +1,13 @@
+package com.du.tery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/du/tery/TeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.tery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

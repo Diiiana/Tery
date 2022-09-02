6d4ef3332230406f747d917777dcc9430bf29ca1
Diana-Cristina Urcan <urcan.diana10@yahoo.com>
2022-09-02 20:16:43 +0300

    Added login data binding

--- src/main/java/com/du/tery/controllers/LoginController.java
+++ src/main/java/com/du/tery/controllers/LoginController.java
@@ -1,13 +1,26 @@
 package com.du.tery.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping("/login")
     public String displayLoginPage() {
         return "login-page.html";
+    }
+
+    @PostMapping("/login")
+    public String authenticateUser(@ModelAttribute("username") String username,
+                                   @ModelAttribute("password") String password) {
+        logger.info("User: " + username + " with " + password + " logging in.");
+        return "welcome-page.html";
     }
 }

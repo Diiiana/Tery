c548e761d0aa17035232ffd37980c9f8357ea5b5
Diana-Cristina Urcan <22178488+Diiiana@users.noreply.github.com>
2024-04-26 21:20:24 +0300

    Add access to stories.

--- src/main/java/com/du/tery/controllers/story/StoryController.java
+++ src/main/java/com/du/tery/controllers/story/StoryController.java
@@ -1,7 +1,6 @@
 package com.du.tery.controllers.story;
 
 import com.du.tery.facade.StoryService;
-import com.du.tery.model.Story;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +19,6 @@ public class StoryController {
 
     @GetMapping("/story/{id}")
     public String getStory(@PathVariable Long id, Model model) {
-        Story s = new Story();
-        s.getId();
         model.addAttribute("story", storyService.getStoryById(id));
         return "story/story-page.html";
     }
